272b435f77333fe0a857bc287ba00fcd6b37878d
christosno <christosno1984@gmail.com>
2023-03-15 23:06:59 +0200

    set up first routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import { Button } from "react-daisyui";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Authentication from "./pages/Authentication";
 
-function App() {
+const RouteLayout = () => {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Geia sou malaka Stelio</h1>
-      <a href="#">Link</a>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
-}
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RouteLayout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "auth", element: <Authentication /> },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
