f5d2ea52f182c0b75e7f6c00e98a25661c09798b
christosno <christosno1984@gmail.com>
2023-04-06 22:46:27 +0300

    init commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Countries from '../components/Leagues/Countries';
-import Matches from '../components/Matches';
+import React, { useState, useEffect } from "react";
+import Countries from "../components/Leagues/Countries";
+import Matches from "../components/Matches";
 
 const Home = () => {
-  // const [data, setData] = useState([]);
-
-  // const options = {
-  //   method: "GET",
-  //   headers: {
-  //     "X-RapidAPI-Key": process.env.REACT_APP_X_RAPIDAPI_KEY,
-  //     "X-RapidAPI-Host": process.env.REACT_APP_X_RAPIDAPI_HOST,
-  //   },
-  // };
-
-  // useEffect(() => {
-  //   // fetchData();
-  // }, []);
-
-  // const fetchData = async () => {
-  //   try {
-  //     const fetchedData = await fetch(
-  //       "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2023-03-15",
-  //       options
-  //     );
-
-  //     const finalData = await fetchedData.json();
-
-  //     console.log(finalData);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
   return (
     <>
       <div className="flex w-auto md:space-x-4 justify-center align-top">
